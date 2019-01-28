59f13e9c8395bc7fd2e85f6073d6f60544377c22
Solo-Hawk <Solo-Hawk@users.noreply.github.com>
2019-01-28 14:24:17 +0000

    Creating Basic Files

---
+++ js/main.js
@@ -0,0 +1,39 @@
+class SceneA extends Phaser.Scene {
+  constructor(){
+    super();
+  }
+  preload(){
+
+  }
+  create(){
+    this.matter.world.setBounds()
+
+    const x = Phaser.Math.Between(0,800)
+    const y = Phaser.Math.Between(0,600)
+    let box = this.matter.add.sprite(x,y,'')
+    box.setBounce(0.99);
+    box.setMass(10);
+    box.setFriction(0.5);
+    this.matter.add.mouseSpring();
+  }
+}
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    pixelArt: true,
+    physics: {
+        default: 'matter',
+        matter: {
+            debug: true,
+            gravity: {
+                y: 1
+            }
+        }
+    },
+    scene: [SceneA]
+};
+
+
+var game = new Phaser.Game(config);
