3a38d8648c7b0d688489348b76aa544106fd1149
Solo-Hawk <Solo-Hawk@users.noreply.github.com>
2019-01-29 10:15:37 +0000

    Small Restructure and work on tilemap
    Stage 1 - Simple Matter Demos
    Stage 2 - Tilemap development

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # matter.js-demo
 A demo in matter.js - I'm trying to start my work in git repos from the start for ease of workflow (I've not been doing a great job since)
+
+# stages
+
+Stage 1 - Light Physics Demo
+Stage 2 - Tilemap development testing
---
+++ Stage 2/js/game.js
@@ -0,0 +1,41 @@
+class SceneA extends Phaser.Scene {
+  constructor(){
+    super();
+  }
+  preload(){
+
+  }
+  create(){
+
+  }
+  update(){
+
+  }
+}
+
+var config = {
+  type: Phaser.AUTO,
+  width: 64*8,
+  height: 64*12,
+  pixelArt: false,
+  physics: {
+    default: 'matter',
+    matter:{
+      debug: true,
+      gravity:{
+        x:0,
+        y:1.5
+      }
+    }
+  },
+  scene: [SceneA],
+  plugins: {
+    scene: [{
+        plugin: PhaserMatterCollisionPlugin, // The plugin class
+        key: "matterCollision", // Where to store in Scene.Systems, e.g. scene.sys.matterCollision
+        mapping: "matterCollision" // Where to store in the Scene, e.g. scene.matterCollision
+      }]
+  }
+}
+
+var game = new Phaser.Game(config)
